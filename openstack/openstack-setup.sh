99ae96aff6f61071b18af87dbf5fb1bee03ca104
unknown <mst@SF2-LAP-MST.recommind.com>
2014-03-11 14:29:18 -0700

    initial commit

---
+++ openstack/openstack-setup.sh
@@ -0,0 +1,4 @@
+sudo yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo yum -y install http://rdo.fedorapeople.org/rdo-release.rpm
+sudo yum -y install openstack-packstack
+sudo packstack --allinone
