8219c9152650dc762ddfea22d59b12a5e7574fb1
Matt Shean <mst@SF2-LAP-MST.recommind.com>
2014-03-12 14:13:37 -0700

    fixes for openstack

--- openstack/openstack-setup.sh
+++ openstack/openstack-setup.sh
@@ -1,4 +1,11 @@
 sudo yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo yum -y install http://rdo.fedorapeople.org/rdo-release.rpm
-sudo yum -y install openstack-packstack
-sudo packstack --allinone
+sudo yum -y install openstack-packstack expect
+
+expect << EOF
+set timeout -1
+spawn "/usr/bin/packstack" "--allinone"
+expect "Setting up ssh keys...root@10.0.2.15's password:"
+send "vagrant\n"
+expect eof
+EOF
