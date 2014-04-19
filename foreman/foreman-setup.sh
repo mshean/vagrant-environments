2e5e28d326f6df080d87b792d89de52f6a2eea2c
Matt Shean <mshean@yahoo-inc.com>
2014-04-19 14:32:14 -0700

    force puppet 3.4.3, 3.5+ breaks foreman 1.4.2

--- foreman/foreman-setup.sh
+++ foreman/foreman-setup.sh
@@ -1,5 +1,5 @@
 sudo yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo yum -y install http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
 sudo yum -y install http://yum.theforeman.org/releases/1.4/el6/x86_64/foreman-release.rpm
-sudo yum -y install foreman-installer
+sudo yum -y install puppet-3.4.3-1.el6 puppet-server-3.4.3-1.el6 foreman-installer
 sudo foreman-installer
